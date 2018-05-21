0728aaf0949f385e11a348d3f6f9063ddd84d60f
Prabhjyot Saluja <prabhjyot10@gmail.com>
2018-05-21 13:16:53 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,2 @@
 node default {
-  file {'/root/README':
-  ensure  => file,
-  content => 'Hello, I hope this works',
-  owner   => 'root',
-  }
-  #file {'/root/README':  Note: Duplicate Declaration doesn't work. Declaring same resource on the same machine twice generates error
-  #ensure  => file,
 }
